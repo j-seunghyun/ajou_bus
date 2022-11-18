c6daa5de7ca8d61b67436e10b49f535b2fb03d63
김기윤 <crakel528@ajou.ac.kr>
2022-11-18 18:01:00 +0900

    add : db atlas connection

--- server/config/express.js
+++ server/config/express.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import secret from './secret.js';
 
 export default () => {
     const app = express();
@@ -14,5 +16,13 @@ export default () => {
     
     // 여기에 라우팅 추가
 
+    // DB connection
+    async function connect() {
+        await mongoose.connect(secret.atlas, { useUnifiedTopology: true, useNewUrlParser: true })
+        .then(() => console.log("MongoDB connected"))
+        .catch((err) => console.log(err));
+    }
+
+    connect();
     return app;
 }
