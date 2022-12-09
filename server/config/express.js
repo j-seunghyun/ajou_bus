8540e4139b98e6b485402443e9850f58a7325c12
김기윤 <crakel528@ajou.ac.kr>
2022-12-09 09:52:54 +0900

    fix : cors setting

--- server/config/express.js
+++ server/config/express.js
@@ -15,7 +15,9 @@ module.exports =  () => {
 
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
-    app.use(cors());
+    app.use(cors({
+        origin: '*',
+    }));
     app.use(cookieParser());
     app.use(session({
         key: secret.sessionKey.key,
