5238993adcd41e77fb962bd465dbf6c20fc69826
jin-Pro <dnjun2@ajou.ac.kr>
2022-12-09 21:04:35 +0900

    fix: axios base url

--- frontend/src/Util/axios.js
+++ frontend/src/Util/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import { getCookie } from "./LocalStorage";
 
 const _ = axios.create({
-  baseURL: "http://ec2-43-201-20-174.ap-northeast-2.compute.amazonaws.com:4000",
+  baseURL: "//ec2-43-201-20-174.ap-northeast-2.compute.amazonaws.com:4000",
+  // baseURL: "http://ec2-43-201-20-174.ap-northeast-2.compute.amazonaws.com:4000",
   headers: {
     token: getCookie(),
   },
