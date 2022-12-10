81dd25d6be922e51e762fa7cde352727b549aacb
jin-Pro <dnjun2@ajou.ac.kr>
2022-12-10 20:29:26 +0900

    fix: add AdminUpdate Hook

---
+++ frontend/src/Pages/AdminPage/AdminUpdate/AdminUpdate.hook.jsx
@@ -0,0 +1,23 @@
+import axios from "../../../Util/axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const useGetNoticeInfo = (id) => {
+  const navigate = useNavigate();
+  const [noticeInfo, setNoticeInfo] = useState();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get(`/api/notice/${id}`)
+      .then((res) => {
+        const info = res.data.result;
+        setNoticeInfo(info);
+        setLoading(false);
+      })
+      .catch((e) => {
+        navigate("/admin");
+      });
+  }, [id]);
+
+  return { loading, noticeInfo };
+};
--- frontend/src/Pages/AdminPage/AdminUpdate/AdminUpdate.jsx
+++ frontend/src/Pages/AdminPage/AdminUpdate/AdminUpdate.jsx
@@ -1,25 +1,10 @@
 import axios from "../../../Util/axios";
-import { useEffect } from "react";
-import { useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useGetNoticeInfo } from "./AdminUpdate.hook";
 
 export const AdminUpdate = () => {
   const { id } = useParams();
-  const navigate = useNavigate();
-  const [noticeInfo, setNoticeInfo] = useState();
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    axios
-      .get(`/api/notice/${id}`)
-      .then((res) => {
-        const info = res.data.result;
-        setNoticeInfo(info);
-        setLoading(false);
-      })
-      .catch((e) => {
-        navigate("/admin");
-      });
-  }, [id]);
+  const { loading, noticeInfo } = useGetNoticeInfo(id);
   if (loading) {
     return <div>...loading</div>;
   }
